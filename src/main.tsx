8480e589b61707f49b8a681c5dae2b318a57842d
Danik <karagodin.danik@gmail.com>
2025-02-12 17:34:24 +0300

    add prettier config

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { HashRouter } from "react-router";
-import { theme } from "@common/theme/theme.tsx";
-import { ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router';
+import { theme } from '@common/theme/theme.tsx';
+import { ThemeProvider } from '@mui/material/styles';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HashRouter>
-      <ThemeProvider theme={theme}>
-        <App></App>
-      </ThemeProvider>
-    </HashRouter>
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <HashRouter>
+            <ThemeProvider theme={theme}>
+                <App></App>
+            </ThemeProvider>
+        </HashRouter>
+    </StrictMode>,
 );
